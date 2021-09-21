fd27ecf1ee88be1cc54cf0e95e08a5a971c58d03
Alex Melnyk <ask.melnyk@gmail.com>
2021-09-21 23:54:18 +0300

    Ensure initialized added to the main.dart file.

--- lib/main.dart
+++ lib/main.dart
@@ -4,21 +4,15 @@ import 'package:theme_switching/pages/pages.dart';
 import 'styles/styles.dart';
 
 void main() {
-  runApp(MyApp());
+  WidgetsFlutterBinding.ensureInitialized();
+
+  Theming.initialize();
+
+  runApp(const MyApp());
 }
 
-class MyApp extends StatefulWidget {
-  @override
-  _MyAppState createState() => _MyAppState();
-}
-
-class _MyAppState extends State<MyApp> {
-  @override
-  void initState() {
-    super.initState();
-
-    Theming.initialize();
-  }
+class MyApp extends StatelessWidget {
+  const MyApp();
 
   @override
   Widget build(BuildContext context) {
